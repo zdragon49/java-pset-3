d1eac4b8d20393a882a27b9769a88b15a3768dc4
Lukas Mikulenas <lmikulenas@ucvts.org>
2020-09-26 20:09:09 -0400

    finished 2

--- src/exercise2.java
+++ src/exercise2.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class exercise2 {
+public class exercise2 {
+    public static void main(String[] args) {
+        String message = " APCSA is the best class ever. Really!" ;
+                System.out.print(message);
+    }
 }
