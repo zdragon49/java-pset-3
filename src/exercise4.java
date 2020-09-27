04ab8d631ec8a08c691be607a03edb4f6835e28a
Lukas Mikulenas <lmikulenas@ucvts.org>
2020-09-27 19:57:28 -0400

    very similar to exercise 3, i think i finished

--- src/exercise4.java
+++ src/exercise4.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class exercise4 {
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+
+public class exercise4 {
+    public static void main(String[] args) {
+        final double inchtocenti = 2.54; // there are 2.54 centimeters in 1 in
+
+        Scanner ni = new Scanner(System.in);
+
+        DecimalFormat formatDecimal = new DecimalFormat("#.00");
+        formatDecimal.setGroupingUsed(true);
+        formatDecimal.setGroupingSize(3);
+
+        System.out.print("Height : ");
+        float h = (float) (ni.nextFloat() * inchtocenti);
+        System.out.print("Width : ");
+        float w = (float) (ni.nextFloat() * inchtocenti);
+
+        float perimeter = (2 * h) + (2 * w);
+
+        System.out.print("\n" + ormatDecimal.format(perimeter) + " centimeters.");
+
+    }
 }
