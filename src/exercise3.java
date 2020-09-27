427f73cff6fbea2ba8c02858843d261202010dba
Lukas Mikulenas <lmikulenas@ucvts.org>
2020-09-27 19:54:21 -0400

    completed exercise3

--- src/exercise3.java
+++ src/exercise3.java
@@ -1,7 +1,23 @@
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class exercise3 {
- public static void main(String[] args) {
-     Scanner in = new Scanner(System.in);
- }
+    public static void main(String[] args) {
+        final double inchtomili = 25.4;  // there are 25.4 mili in 1 inch
+
+        Scanner ni = new Scanner(System.in);
+
+        DecimalFormat formatDecimal = new DecimalFormat("#.00");
+        formatDecimal.setGroupingUsed(true);
+        formatDecimal.setGroupingSize(3);
+
+        System.out.print("Height : ");
+        float h = (float) (ni.nextFloat() * inchtomili);
+        System.out.print("Width : ");
+        float w = (float) (ni.nextFloat() * inchtomili);
+
+        float area = h * w;
+
+        System.out.print("\n" + formatDecimal.format(area) + " square millimeters.");
+    }
 }
