448fe1f67e1def8e4df927b248598e439968e3d3
Lukas Mikulenas <lmikulenas@ucvts.org>
2020-09-27 19:45:03 -0400

    added a new message

--- src/exercise2.java
+++ src/exercise2.java
@@ -1,6 +1,6 @@
 public class exercise2 {
     public static void main(String[] args) {
-        String message = " APCSA is the best class ever. Really!" ;
+        String message = "\"The answer's quite simple,\" the professor said. \"Just think outside the box!\"" ; 
                 System.out.print(message);
     }
 }
