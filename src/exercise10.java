1415f7951a8f357f416eded716082a562e294f82
Lukas Mikulenas <lmikulenas@ucvts.org>
2020-09-27 20:38:55 -0400

    finished exercise 10

---
+++ src/exercise10.java
@@ -0,0 +1,22 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class exercise10 {
+    public static void main(String[] args) {
+        Scanner ni = new Scanner(System.in);
+        DecimalFormat formatDecimal = new DecimalFormat("0.00");
+
+        formatDecimal.setGroupingUsed(true);
+        formatDecimal.setGroupingSize(3);
+
+        System.out.print("Fahrenheit : ");
+        double f = ni.nextDouble();
+
+        double k = ((f - 32) * (5/9)) + 273.15;
+        double c = k - 273.15;
+
+        System.out.println("\nCelsius    : " + formatDecimal.format(c));
+        System.out.print("Kelvin     : " + formatDecimal.format(k));
+
+    }
+}
