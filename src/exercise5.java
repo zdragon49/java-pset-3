cf16737b2a184dae6a5f029ebde9454d8547d97f
Lukas Mikulenas <lmikulenas@ucvts.org>
2020-09-27 20:09:15 -0400

    completed exercise 5

--- src/exercise5.java
+++ src/exercise5.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class exercise5 {
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class exercise5 {
+    public static void main(String[] args) {
+
+        Scanner ni = new Scanner(System.in);
+        DecimalFormat formatDecimal = new DecimalFormat("#.00");
+
+        final double hwweight = 0.15;
+        final double quizweight = 0.35;
+        final double testweight = 0.5;
+
+        System.out.print("Homework 1 : ");
+        double hw1 = ni.nextDouble();
+        System.out.print("Homework 2 : ");
+        double hw2 = ni.nextDouble();
+        System.out.print("Homework 3 : ");
+        double hw3 = ni.nextDouble();
+        System.out.print("Quiz 1     : ");
+        double quiz1 = ni.nextDouble();
+        System.out.print("Quiz 2     : ");
+        double quiz2 = ni.nextDouble();
+        System.out.print("Test 1     : ");
+        double test1 = ni.nextDouble();
+
+        double hwgrade = ((hw1 + hw2 + hw3) / 3) * hwweight;
+        double quizgrade = ((quiz1 + quiz2) / 2) * quizweight;
+        double testgrade = test1 * testweight;
+
+        double final_grade = hwgrade + quizgrade + testgrade;
+
+        System.out.print("\n" + formatDecimal.format(final_grade) + "%.");
+    }
 }
