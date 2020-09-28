10ae6e65bc5b5d89067131a660a94cbda7656791
Lukas Mikulenas <lmikulenas@ucvts.org>
2020-09-27 20:39:26 -0400

    fixed and completed mistakes for exercise 9

--- src/exercise9.java
+++ src/exercise9.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class exercise9 {
+import java.util.Scanner;
+
+public class exercise9 {
+    public static void main(String[] args) {
+        Scanner ni = new Scanner(System.in);
+
+        System.out.print("Amount : ");
+        double amoun = ni.nextDouble();
+
+        amoun = amoun * 100;
+
+        int quat = (int) amoun / 25;
+        int left = (int) (amoun % 25);
+        int dime = left / 10;
+        left = left % 10;
+        int nick = left / 5;
+        left = left % 5;
+        int penn = left;
+
+        System.out.println("\nQuarters : " + quat);
+        System.out.println("Dimes    : " + dime);
+        System.out.println("Nickels  : " + nick);
+        System.out.print("Pennies  : " + penn);
+
+    }
 }
