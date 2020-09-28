43ed172572ef72a0cf2847953ecacc33320dccd9
Lukas Mikulenas <lmikulenas@ucvts.org>
2020-09-27 20:32:39 -0400

    finished and fixed some minor mistakes

--- src/exercise8.java
+++ src/exercise8.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class exercise8 {
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class exercise8 {
+    public static void main(String[] args) {
+        Scanner ni = new Scanner(System.in);
+        DecimalFormat formatDecimal = new DecimalFormat("#.00");
+
+        System.out.print("Students : ");
+        int studnum = ni.nextInt();
+        System.out.print("Teachers : ");
+        int teachernum = ni.nextInt();
+        System.out.print("Capacity : ");
+        int totalcapacity = ni.nextInt();
+
+        int passangerstotalnum = studnum + teachernum;
+        int busesnum = passangerstotalnum / totalcapacity;
+        int extra = passangerstotalnum % totalcapacity;
+        System.out.println("\n" + "Buses required      : " + formatDecimal.format(busesnum + 1));
+        System.out.print("Overflow passengers : " + extra);
+    }
 }
