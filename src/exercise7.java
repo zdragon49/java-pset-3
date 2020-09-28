c92f955f3904d5932c6108b4086280508efdedce
Lukas Mikulenas <lmikulenas@ucvts.org>
2020-09-27 20:27:03 -0400

    started and completed exercise7.java:

--- src/exercise7.java
+++ src/exercise7.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class exercise7 {
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class exercise7 {
+    public static void main(String[] args) {
+        Scanner ni = new Scanner(System.in);
+        DecimalFormat formatDecimal = new DecimalFormat("#.00");
+
+        formatDecimal.setGroupingUsed(true);
+        formatDecimal.setGroupingSize(3);
+
+        System.out.print("Salary  : ");
+        double salary = ni.nextDouble();
+        System.out.print("401(k)  : ");
+        double k401tax = ni.nextDouble() / 100;
+        System.out.print("Federal : ");
+        double feds = ni.nextDouble() / 100;
+        System.out.print("State   : ");
+        double state = ni.nextDouble() / 100;
+        double salaryfinal = salary - (salary * k401tax);
+        salaryfinal = salaryfinal - (salaryfinal * (feds + state));
+
+        double salarybiweek = salaryfinal / 24;
+
+        System.out.print("\n$" + formatDecimal.format(salarybiweek) + ".");
+
+    }
 }
