1dcac999b498f96a19ee743521e0aa3a14067d69
Lukas Mikulenas <lmikulenas@ucvts.org>
2020-10-06 19:37:43 -0400

    revisions for exercise7

--- src/Exercise7.java
+++ src/Exercise7.java
@@ -3,26 +3,25 @@ import java.util.Scanner;
 
 public class Exercise7 {
     public static void main(String[] args) {
-        Scanner ni = new Scanner(System.in);
-        DecimalFormat formatDecimal = new DecimalFormat("#.00");
-
-        formatDecimal.setGroupingUsed(true);
-        formatDecimal.setGroupingSize(3);
-
+        Scanner in = new Scanner(System.in);
         System.out.print("Salary  : ");
-        double salary = ni.nextDouble();
+        double salary = in.nextDouble();
         System.out.print("401(k)  : ");
-        double k401tax = ni.nextDouble() / 100;
+        double k401 = in.nextDouble();
         System.out.print("Federal : ");
-        double feds = ni.nextDouble() / 100;
+        double feds = in.nextDouble();
         System.out.print("State   : ");
-        double state = ni.nextDouble() / 100;
-        double salaryfinal = salary - (salary * k401tax);
-        salaryfinal = salaryfinal - (salaryfinal * (feds + state));
+        double state = in.nextDouble();
+        double taxPost = (salary * ((100 - k401) / 100));
+        double homeTake = taxPost - (taxPost * (feds / 100)) - (taxPost * (state / 100));
+        double paycheck = homeTake / 24;
 
-        double salarybiweek = salaryfinal / 24;
+        DecimalFormat commaFormat;
+        commaFormat = new DecimalFormat("#,###.##");
 
-        System.out.print("\n$" + formatDecimal.format(salarybiweek) + ".");
+        System.out.println(" ");
+        System.out.println("$" + (commaFormat.format(paycheck)) + ".");
 
+        in.close();
     }
 }
