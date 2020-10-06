c53df6817b879d90eedebcfe291b2779898822f6
Lukas Mikulenas <lmikulenas@ucvts.org>
2020-10-06 19:35:43 -0400

    revisions for exercise6

--- src/Exercise6.java
+++ src/Exercise6.java
@@ -4,38 +4,30 @@ import java.util.Scanner;
 public class Exercise6 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
         System.out.print("Wage      : ");
         double wage = in.nextDouble();
-
         System.out.print("Monday    : ");
-        double monday = in.nextDouble();
-
+        double mon = in.nextDouble();
         System.out.print("Tuesday   : ");
-        double tuesday = in.nextDouble();
-
+        double tues = in.nextDouble();
         System.out.print("Wednesday : ");
-        double wednesday = in.nextDouble();
-
+        double wed = in.nextDouble();
         System.out.print("Thursday  : ");
-        double thursday = in.nextDouble();
-
+        double thur = in.nextDouble();
         System.out.print("Friday    : ");
-        double friday = in.nextDouble();
-
+        double fri = in.nextDouble();
         System.out.print("Saturday  : ");
-        double saturday = in.nextDouble();
-
+        double sat = in.nextDouble();
         System.out.print("Sunday    : ");
-        double sunday = in.nextDouble();
+        double sun = in.nextDouble();
 
-        double grosspay = (monday * wage) + (tuesday * wage) + (wednesday * wage) + (thursday * wage) + (friday * wage) + (saturday * wage) + (sunday * wage);
+        double payGross = (mon * wage) + (tues * wage) + (wed * wage) + (thur * wage) + (fri * wage) + (sat * wage) + (sun * wage);
 
         NumberFormat defaultFormat = NumberFormat.getCurrencyInstance();
         defaultFormat.setMinimumFractionDigits(2);
 
         System.out.println(" ");
-        System.out.println((defaultFormat.format(grosspay)) + ".");
+        System.out.println((defaultFormat.format(payGross)) + ".");
 
         in.close();
     }
