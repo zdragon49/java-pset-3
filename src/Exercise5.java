f93b4f78213e4b16fb6da954219ea8a07a337512
Lukas Mikulenas <lmikulenas@ucvts.org>
2020-10-06 19:36:06 -0400

    revisions for exercise5.java

--- src/Exercise5.java
+++ src/Exercise5.java
@@ -1,35 +1,31 @@
 import java.util.Scanner;
-import java.text.DecimalFormat;
 
 public class Exercise5 {
     public static void main(String[] args) {
-
-        Scanner ni = new Scanner(System.in);
-        DecimalFormat formatDecimal = new DecimalFormat("#.00");
-
-        final double hwweight = 0.15;
-        final double quizweight = 0.35;
-        final double testweight = 0.5;
-
+        Scanner in = new Scanner(System.in);
         System.out.print("Homework 1 : ");
-        double hw1 = ni.nextDouble();
+        double hw1 = in.nextDouble();
         System.out.print("Homework 2 : ");
-        double hw2 = ni.nextDouble();
+        double hw2 = in.nextDouble();
         System.out.print("Homework 3 : ");
-        double hw3 = ni.nextDouble();
+        double hw3 = in.nextDouble();
         System.out.print("Quiz 1     : ");
-        double quiz1 = ni.nextDouble();
+        double quiz1 = in.nextDouble();
         System.out.print("Quiz 2     : ");
-        double quiz2 = ni.nextDouble();
+        double quiz2 = in.nextDouble();
         System.out.print("Test 1     : ");
-        double test1 = ni.nextDouble();
+        double test1 = in.nextDouble();
+        double homeworkavg = (hw1 + hw2 + hw3)/3;
+        double quizavg = (quiz1 + quiz2)/2;
+        double testavg = test1;
 
-        double hwgrade = ((hw1 + hw2 + hw3) / 3) * hwweight;
-        double quizgrade = ((quiz1 + quiz2) / 2) * quizweight;
-        double testgrade = test1 * testweight;
+        double gradeFinal = (homeworkavg * .15) + (quizavg * .35) + (testavg * .5);
 
-        double final_grade = hwgrade + quizgrade + testgrade;
+        String strDouble = String.format("%.2f", gradeFinal);
 
-        System.out.print("\n" + formatDecimal.format(final_grade) + "%.");
+        System.out.println(" ");
+        System.out.println((strDouble) + "%");
+
+        in.close();
     }
 }
