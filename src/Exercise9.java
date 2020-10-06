0b664f5c5df30125ebe40a6c75112486a1c04c24
Lukas Mikulenas <lmikulenas@ucvts.org>
2020-10-06 19:41:56 -0400

    completed revisions for exercise 9

--- src/Exercise9.java
+++ src/Exercise9.java
@@ -2,25 +2,35 @@ import java.util.Scanner;
 
 public class Exercise9 {
     public static void main(String[] args) {
-        Scanner ni = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 
         System.out.print("Amount : ");
-        double amoun = ni.nextDouble();
+        double amount = in.nextDouble();
 
-        amoun = amoun * 100;
+        int quarter = 25;
+        int dime = 10;
+        int nickel = 5;
 
-        int quat = (int) amoun / 25;
-        int left = (int) (amoun % 25);
-        int dime = left / 10;
-        left = left % 10;
-        int nick = left / 5;
-        left = left % 5;
-        int penn = left;
+        double change = amount * 100;
+        double change1 = change % quarter;
+        double quarters = (change - change1) / quarter;
+        double change2 = change1 % dime;
+        double dimes = (change1 - change2) / dime;
+        double change3 = change2 % nickel;
+        double nickels = (change2 - change3) / nickel;
+        double penn = change3;
 
-        System.out.println("\nQuarters : " + quat);
-        System.out.println("Dimes    : " + dime);
-        System.out.println("Nickels  : " + nick);
-        System.out.print("Pennies  : " + penn);
+        String var1 = String.format("%.0f", quarters);
+        String var2 = String.format("%.0f", dimes);
+        String var3 = String.format("%.0f", nickels);
+        String var4 = String.format("%.0f", penn);
 
+        System.out.println(" ");
+        System.out.println("Quarters : " + var1);
+        System.out.println("Dimes    : " + var2);
+        System.out.println("Nickels  : " + var3);
+        System.out.println("Pennies  : " + var4);
+
+        in.close();
     }
 }
