aedc162d90af1d4df71d587b2f97bad1b683d3d6
Lukas Mikulenas <lmikulenas@ucvts.org>
2020-10-06 19:39:40 -0400

    revisions for exercise8

--- src/Exercise8.java
+++ src/Exercise8.java
@@ -1,22 +1,24 @@
-import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Exercise8 {
     public static void main(String[] args) {
-        Scanner ni = new Scanner(System.in);
-        DecimalFormat formatDecimal = new DecimalFormat("#.00");
-
+        Scanner in = new Scanner(System.in);
         System.out.print("Students : ");
-        int studnum = ni.nextInt();
+        double students = in.nextDouble();
         System.out.print("Teachers : ");
-        int teachernum = ni.nextInt();
+        double teachers = in.nextDouble();
         System.out.print("Capacity : ");
-        int totalcapacity = ni.nextInt();
+        double cap = in.nextDouble();
+        double passengers = students + teachers;
+        double extra = passengers % cap;
+        double buses = ((passengers - extra) / cap) + 1;
+        String string1 = String.format("%.0f", buses);
+        String string2 = String.format("%.0f", extra);
 
-        int passangerstotalnum = studnum + teachernum;
-        int busesnum = passangerstotalnum / totalcapacity;
-        int extra = passangerstotalnum % totalcapacity;
-        System.out.println("\n" + "Buses required      : " + formatDecimal.format(busesnum + 1));
-        System.out.print("Overflow passengers : " + extra);
+        System.out.println(" ");
+        System.out.println("Buses required      : " + string1);
+        System.out.println("Overflow passengers : " + string2);
+
+        in.close();
     }
 }
