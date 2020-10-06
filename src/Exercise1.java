eb428ec8ac0ccc02d3876174b571700f2810994d
Lukas Mikulenas <lmikulenas@ucvts.org>
2020-10-06 17:30:31 -0400

    revisions

---
+++ src/Exercise1.java
@@ -0,0 +1,10 @@
+public class Exercise1{
+    public static void main(String[] args) {
+        System.out.println("H E L L O" +
+                "\nE       A" +
+                "\nL       P" +
+                "\nL       C" +
+                "\nO A P C S"
+        );
+    }
+}
