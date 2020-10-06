1ef36a28e20eefac70655fd73686ccc9e5789580
Lukas Mikulenas <lmikulenas@ucvts.org>
2020-10-06 17:31:01 -0400

    revisions for exercise2

--- src/Exercise2.java
+++ src/Exercise2.java
@@ -1,6 +1,5 @@
-public class Exercise2 {
+public class Exercise2{
     public static void main(String[] args) {
-        String message = "\"The answer's quite simple,\" the professor said. \"Just think outside the box!\"" ;
-                System.out.print(message);
+        System.out.println("\"The answer's quite simple,\" the professor said. \"Just think outside the box!\"");
     }
 }
